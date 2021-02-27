de72a7b5045b9ff221e0a9b9222f68dd768b3088
Alex Truong <haiyen@student.ubc.ca>
2021-02-26 20:03:10 -0800

    Combine in single function, add option to scape online or local, add option to export csv

--- pyhousehunter/scraper.py
+++ pyhousehunter/scraper.py
@@ -1,75 +1,63 @@
 # author: Alex Truong Hai Yen
 # date: 2021-02-25
 
-"""""Scrape housing data from a give craiglist url and export result to csv file.
-
-Usage: pyhousehunter/scraper.py --url=<url> 
-
-Options:
---url=<url>              The chosen craiglist url 
-"""
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import csv
-from docopt import docopt
-
-opt = docopt(__doc__)
-
-
-### PART1: create soup object from the url
-def make_soup(url):
+ 
+def scraper(url, online = False, save_csv = False):
     """Function to scrape housing data from a given craiglist url
 
     Parameters
     ----------
     url : url
-        The given craiglist url
-
-    Returns
-    -------
-    bs4.BeautifulSoup
-        The soup object from scraping the url
-    """
-    headers = {
-        'DNT': '1',
-        'Referer': 'https://vancouver.craigslist.org/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    }
-
-    page = requests.get(url, headers = headers)
-
-    # checking scrape status
-    if page.status_code == 200:
-        print("OK")
-    else: 
-        print(page.status_code)
-
-    soup = BeautifulSoup(page.text, 'html.parser')
+        The given craiglist url to scrape the data from
     
-    return soup
+    online: bool
+        Whether the data is scraped directly online from the url (default = False) 
+        False means the data is scraped from local HTML file
 
-
-### PART2: extracting information from scrape results and export to csv
-def extractor(soup): 
-    """Extract information from the soup object and export result to csv
-
-    Parameters
-    ----------
-    soup : bs4.BeautifulSoup
-        The soup object from scraping the url
+    save_csv: bool
+        Whether to export the data to CSV file (default = False)
 
     Returns
     -------
-        a csv file contained listing information such as price, house type and neighborhood
+    pandas.core.frame.DataFrame
+        A dataframe containing listing information such as price, house type and neighborhood. 
+        A csv containing the dataframe could also be exported with `save_csv` option is 
+        activated to True.
 
+    Examples
+    -------
+    >>> scraper(url = 'https://vancouver.craigslist.org/search/van/apa', save_csv = True)
     """
-       
+
+    # PART 1: create soup object either from the url or local HTML file
+    if online:
+        headers = {
+            'DNT': '1',
+            'Referer': 'https://vancouver.craigslist.org/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+
+        page = requests.get(url, headers = headers)
+
+        # checking scrape status
+        if page.status_code == 200:
+            print("OK")
+        else: 
+            print(page.status_code)
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+    else:
+        soup = BeautifulSoup(open("pyhousehunter/temp/van_housing_listings.html"), "html.parser") # local scraping
+
+
+    # PART2: extracting information from scrape results into dataframe
     listings = soup.find_all('div', attrs = {'class': 'result-info'})
     data = []
 
-    # extract data from soup object
     for i in range(len(listings)):
         listing_id = listings[i].find('a').get('data-id')
         listing_url = listings[i].find('a').get('href')
@@ -78,22 +66,15 @@ def extractor(soup):
         neighborhood = listings[i].find('span', attrs = {'class': 'result-hood'})
 
         data.append((listing_id, listing_url, price, house, neighborhood))
+
+    output = pd.DataFrame(
+        data,
+        columns = ['listing_id', 'listing_url', 'price', 'house_type', 'neighborhood']
+    )
     
-    # writing results to CSV file
-    with open('raw.csv', 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        for listing_id, listing_url, price, house, neighborhood in data:
-            writer.writerow([listing_id, listing_url, price, house, neighborhood])
 
+    # PART3: activate the option to write results to CSV file
+    if save_csv:
+        output.to_csv('raw.csv', index_label = "serial_no")
 
-### PART3: the main function      
-def main(url):
-    soup = BeautifulSoup(open("pyhousehunter/temp/van_housing_listings.html"), "html.parser") # local scraping
-    #soup = make_soup(url) # online scraping
-    data = extractor(soup)
-
-
-if __name__ == "__main__":
-  main(opt["--url"])
-  
-
+    return output
