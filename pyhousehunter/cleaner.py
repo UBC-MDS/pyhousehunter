a3721e6b927c4d01e41dffd06dc06d52a15e8af7
JuntingHe <junting.heee@gmail.com>
2021-03-05 19:52:37 -0800

    Modify cleaner function

--- pyhousehunter/cleaner.py
+++ pyhousehunter/cleaner.py
@@ -7,45 +7,54 @@ import re
 from geotext import GeoText
 from operator import attrgetter
 
-def data_cleaner(raw_data_filepath, save_to_csv = False):
+def data_cleaner(scraped_df):
     """A function to clean web-scraped data with Pandas and Regex.
 
     Parameters
     ----------
-    raw_data_filepath : str
-        The relative path of the raw data CSV file.
-    save_to_csv: bool
-        Whether to export the cleaned data into a CSV file (default = False)
+    scraped_df: pandas.core.frame.DataFrame
+        A dataframe containing web-scraped data like listing url, price and house type. 
 
     Returns
     -------
     pandas.core.frame.DataFrame
-        A cleaned dataframe ready for filtering and analysis. 
-        A csv containing the cleaned dataframe could also be exported with `save_to_csv` option is True.
+        A cleaned dataframe containing ininformation like listing url, price, number of bedroom, area in sqft, and city.
     
 
     Examples
     --------
-    >>> data_cleaner("raw.csv")
+    >>> data_cleaner(scraped_df)
     """ 
     
-    ### read the data into a panda dataframe and make a copy of it
-    df = pd.read_csv(raw_data_filepath)
-    data = df.copy()
+    if not isinstance(scraped_df, pd.DataFrame):
+        raise ValueError("Invalid input. Please enter a dataframe object")
+
+    if not 'price' in scraped_df.columns:
+        raise ValueError("Missing price column in the input dataframe")
+
+    if not 'house_type' in scraped_df.columns:
+        raise ValueError("Missing house_type column in the input dataframe")
+
+    if not 'listing_url' in scraped_df.columns:
+        raise ValueError("Missing price listing_url column in the input dataframe")
+   
+    data = scraped_df.copy()
     
     ### convert the price column into numerice data types
     data['price'] = data['price'].str.replace('$', '', regex=False).str.replace(',', '')
     data['price'] = pd.to_numeric(data['price'])
     
     ### extract the information about the number of bedroom of the housing 
-    data['bed_num'] = data['house_type'].str.extract(r'([0-9]+[b]{1}[r]{1})')
-    data['bed_num'] = data['bed_num'].str.replace('br', '')
-    data['bed_num'] = pd.to_numeric(data['bed_num'])
+    data['num_bedroom'] = data['house_type'].str.extract(r'([0-9]+[b]{1}[r]{1})')
+    data['num_bedroom'] = data['num_bedroom'].str.replace('br', '')
+    data['num_bedroom'] = pd.to_numeric(data['num_bedroom'])
+    data['num_bedroom'] = data['num_bedroom'].astype('Int64')
 
     ### extract the information about the area of the housing
-    data['area(ft2)'] = data['house_type'].str.extract(r'([0-9]+[f]{1}[t]{1}[2]{1})')
-    data['area(ft2)'] = data['area(ft2)'].str.replace('ft2', '')
-    data['area(ft2)'] = pd.to_numeric(data['area(ft2)'])
+    data['area_sqft'] = data['house_type'].str.extract(r'([0-9]+[f]{1}[t]{1}[2]{1})')
+    data['area_sqft'] = data['area_sqft'].str.replace('ft2', '')
+    data['area_sqft'] = pd.to_numeric(data['area_sqft'])
+    data['area_sqft'] = data['area_sqft'].astype('Int64')
     
     ### extract the information about which city are the housing located in 
     data['city'] = data['listing_url'].str.extract(r'([d]{1}[/]{1}[a-z]+[-]{1})')
@@ -53,11 +62,9 @@ def data_cleaner(raw_data_filepath, save_to_csv = False):
     data['city'] = data['city'].apply(GeoText).apply(attrgetter('cities'))
     data['city']  = data['city'].apply(lambda x: np.nan if len(x)==0 else x).str[0]
     
-    ### drop the column which is not useful for future filtering
-    data.drop(columns=['listing_id', 'house_type', 'neighborhood'], inplace=True)
+    ### select the columns which are useful for future filtering
+    cleaned_data = data[["listing_url", "price", "num_bedroom", "area_sqft", "city"]]
     
     ### activate the option to write results to CSV file
-    if save_to_csv:
-        data.to_csv('cleaned_data.csv', index_label = "serial_no")
 
-    return data
+    return cleaned_data
